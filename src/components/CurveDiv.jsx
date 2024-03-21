1176ab122a5d1150b1a637429dc3504621902871
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-03-21 22:10:28 +0530

    Hero Section Bottom design

---
+++ src/components/CurveDiv.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './CurveDiv.css'
+
+function CurveDiv() {
+  return (
+    <div className=" relative w-full h-[100px]">
+      <div className="inner-div">
+        {" "}
+      </div>
+    </div>
+  );
+}
+
+export default CurveDiv;
