b2e8ca2df6a5e1999be49fc801209406b25ade0e
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-03-21 22:11:09 +0530

    Deleted

--- src/components/HeroSection3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HeroSection3() {
-  return (
-    <div className='w-full h-screen bg-blue-600 flex justify-center items-center'>HeroSection3</div>
-  )
-}
-
-export default HeroSection3
